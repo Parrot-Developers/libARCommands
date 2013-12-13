3b947cb9ef40f6bbbfd4dc4ff6ca4be9d5a04e3a
Sébastien Monat <sebastien.monat.ext@parrot.com>
2013-12-13 11:55:57 +0100

    [CONFIG] Rename mykonos3 to dragon on atom.mk

--- Build/atom.mk
+++ Build/atom.mk
@@ -5,7 +5,7 @@ ifneq ("$(TARGET_LIBC)","bionic")
 
 include $(CLEAR_VARS)
 
-LOCAL_CATEGORY_PATH := mykonos3/libs
+LOCAL_CATEGORY_PATH := dragon/libs
 LOCAL_MODULE := libARCommands
 LOCAL_DESCRIPTION := ARSDK Piloting and User Commands
 
