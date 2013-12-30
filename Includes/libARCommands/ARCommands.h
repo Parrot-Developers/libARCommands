fdc09cd65dcc6fa4d6541d63a614a8a48dc28f1c
Frédéric D'HAEYER <frederic.dhaeyer@parrot.com>
2013-12-30 14:22:30 +0100

    Add global header of libARCommands.

---
+++ Includes/libARCommands/ARCommands.h
@@ -0,0 +1,13 @@
+/**
+ * @file libARCommands/ARCommands.h
+ * @brief libARCommands global header.
+ * This file contains all headers of libARCommands
+ **/
+#ifndef _LIBARCOMMANDS_ARCOMMANDS_H_
+#define _LIBARCOMMANDS_ARCOMMANDS_H_
+
+#include <libARCommands/ARCOMMANDS_Decoder.h>
+#include <libARCommands/ARCOMMANDS_Generator.h>
+#include <libARCommands/ARCOMMANDS_Types.h>
+
+#endif /* _LIBARCOMMANDS_ARCOMMANDS_H_ */
