84409504aeba8d28e73f2a62c89fe63d0a590684
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-01-22 17:24:12 +0100

    [BUILD] Do not use autotools anymore, just alchemy build
    Change-Id: I68893db6cb95317beb83b8201e389dc12340500e
    Reviewed-on: https://gerrit.parrot.biz/28565
    Reviewed-by: Nicolas Christe <nicolas.christe@parrot.com>

--- atom.mk
+++ atom.mk
@@ -2,46 +2,33 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CATEGORY_PATH := dragon/libs
 LOCAL_MODULE := libARCommands
 LOCAL_DESCRIPTION := ARSDK Piloting and User Commands
+LOCAL_CATEGORY_PATH := dragon/libs
 
-LOCAL_LIBRARIES := ARSDKBuildUtils libARSAL
-LOCAL_EXPORT_LDLIBS := -larcommands
+LOCAL_MODULE_FILENAME := libarcommands.so
 
-# Copy in build dir so bootstrap files are generated in build dir
-LOCAL_AUTOTOOLS_COPY_TO_BUILD_DIR := 1
+LOCAL_LIBRARIES := libARSAL
 
-# Configure script is not at the root
-LOCAL_AUTOTOOLS_CONFIGURE_SCRIPT := Build/configure
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/Includes \
+	$(LOCAL_PATH)/gen/Includes \
+	$(LOCAL_PATH)/Sources
 
-# Autotools variable
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS := \
-	--with-libARSALInstallDir=""
+LOCAL_SRC_FILES := \
+	gen/Sources/ARCOMMANDS_Generator.c \
+	gen/Sources/ARCOMMANDS_Decoder.c \
+	gen/Sources/ARCOMMANDS_Filter.c \
+	gen/Sources/ARCOMMANDS_ReadWrite.c \
+	Sources/ARCOMMANDS_Version.c
 
-ifeq ("$(TARGET_OS_FLAVOUR)","android")
+LOCAL_INSTALL_HEADERS := \
+	gen/Includes/libARCommands/ARCOMMANDS_Generator.h:usr/include/libARCommands/ \
+	gen/Includes/libARCommands/ARCOMMANDS_Decoder.h:usr/include/libARCommands/ \
+	gen/Includes/libARCommands/ARCOMMANDS_Types.h:usr/include/libARCommands/ \
+	gen/Includes/libARCommands/ARCOMMANDS_Filter.h:usr/include/libARCommands/ \
+	Includes/libARCommands/ARCOMMANDS_Version.h:usr/include/libARCommands/ \
+	gen/Includes/libARCommands/ARCOMMANDS_Ids.h:usr/include/libARCommands/ \
+	Includes/libARCommands/ARCommands.h:usr/include/libARCommands/
 
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--disable-static \
-	--enable-shared \
-	--disable-so-version
-
-else ifneq ($(filter iphoneos iphonesimulator, $(TARGET_OS_FLAVOUR)),)
-
-LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
-	--enable-static \
-	--disable-shared \
-	CCASFLAGS=" -x assembler-with-cpp -std=gnu99 $(TARGET_GLOBAL_CFLAGS)" \
-	OBJCFLAGS=" -x objective-c -fobjc-arc -std=gnu99 $(TARGET_GLOBAL_CFLAGS)" \
-	OBJC="$(TARGET_CC)" \
-	CFLAGS=" -std=gnu99 -x c $(TARGET_GLOBAL_CFLAGS)"
-
-endif
-
-# User define command to be launch before configure step.
-# Generates files used by configure
-define LOCAL_AUTOTOOLS_CMD_POST_UNPACK
-	$(Q) cd $(PRIVATE_SRC_DIR)/Build && ./bootstrap
-endef
-
-include $(BUILD_AUTOTOOLS)
+include $(BUILD_LIBRARY)
