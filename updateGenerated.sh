dcf7e362a272f5e522a736c0b593c2d1bb64c2be
Jean-Baptiste Dubois <jean-baptiste.dubois@parrot.com>
2016-02-02 09:54:50 +0100

    [DEV] remove gen directory before regenerate it
    Change-Id: Ic1ca3a93168ec2357a2bb6e94c758c41f439234f
    Reviewed-on: https://gerrit.parrot.biz/29162
    Reviewed-by: Djavan Bertrand <djavan.bertrand@parrot.com>

--- updateGenerated.sh
+++ updateGenerated.sh
@@ -1,4 +1,5 @@
 #!/bin/sh 
 
+rm -rf ./gen
 python ./Xml/generateLibARCommands.py -debug-cmds yes -projects all || exit 1
 python ../ARSDKBuildUtils/Utils/Python/ARSDK_PrebuildActions.py --lib libARCommands --root . || exit 1
